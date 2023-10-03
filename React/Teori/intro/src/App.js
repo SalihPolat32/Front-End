73ce0c2852c2dcc59cf3952666c33ecd7b72d8e6
Salih Polat <97470567+SalihPolat32@users.noreply.github.com>
2023-10-03 17:13:02 +0300

    03.10.2023 Gün Sonu

--- React/Teori/intro/src/App.js
+++ React/Teori/intro/src/App.js
@@ -1,24 +1,43 @@
+import React from "react";
+
+import Anchor from "./components/Anchor";
+import Paragraph from "./components/Paragraph";
+import Image from "./components/Image";
+import Header from "./components/Header";
+import ConditionalButton from "./components/ConditionalButton";
+
 import logo from "./logo.svg";
 import "./App.scss";
 
 function App() {
-  const style = { display: "none", backgroundColor: "blue" };
+  const style = { display: "block" /*, backgroundColor: "blue"*/ };
+
+  const onClickParagraph = () => {
+    console.log("clicked");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p style={style}>
+      <Header className="App-header">
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Paragraph
+          style={style}
+          className="App-paragraph"
+          onClick={onClickParagraph}
+        >
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </Paragraph>
+        <Anchor
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          purple // purple={true}
         >
           Learn React
-        </a>
-      </header>
+        </Anchor>
+        <ConditionalButton />
+      </Header>
     </div>
   );
 }
